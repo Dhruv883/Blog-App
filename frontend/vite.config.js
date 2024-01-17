c120432dd0efcde1159ab8a50eccbf9a73bc4adb
Dhruv Dedhia <dhruvdedhia03@gmail.com>
2024-01-17 11:22:36 +0530

    Vite Config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  vite: {
+    server: {
+      watch: {
+        usePolling: true,
+      },
+    },
+
+    paths: {
+      "*": ["*", "*.jsx"],
+    },
+  },
+});
